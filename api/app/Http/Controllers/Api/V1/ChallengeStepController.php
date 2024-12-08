7ef8e3dbd53af185041899828c7a0af02b55a2e7
Lyx Rothböck <lypsilonx@gmail.com>
2024-12-08 20:22:04 +0100

    api routes

---
+++ api/app/Http/Controllers/Api/V1/ChallengeStepController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Requests\StoreChallengeStepRequest;
+use App\Http\Requests\UpdateChallengeStepRequest;
+use App\Models\ChallengeStep;
+use App\Http\Controllers\Controller;
+
+class ChallengeStepController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreChallengeStepRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ChallengeStep $challengeStep)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ChallengeStep $challengeStep)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateChallengeStepRequest $request, ChallengeStep $challengeStep)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ChallengeStep $challengeStep)
+    {
+        //
+    }
+}
---
+++ api/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Api\V1\ChallengeController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// api/v1
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
+    Route::apiResource('challenges', ChallengeController::class);
+    Route::get('challengeSteps/{challenge}', [ChallengeController::class, 'steps']);
+});
