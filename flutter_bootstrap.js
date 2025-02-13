83508e112e173fed79221712e8fe216921548520
Lyx Rothböck <lypsilonx@gmail.com>
2025-02-13 17:53:03 +0100

    build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2630080290"
+    serviceWorkerVersion: "299736811"
   }
 });
